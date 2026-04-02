840527e887bd3c35ac301788f2310beb12e1cc5b
Dan Platon <dan@hypeit.agency>
2026-04-02 09:28:41 +0300

    feat(08-01): add TenancyTestKernel and MakeTenancyTestServicesPublicPass
    - TenancyTestKernel: database-mode kernel (tenancy_test env) with TenantConnection wrapperClass
    - MakeTenancyTestServicesPublicPass: exposes tenancy.context, tenancy.bootstrapper_chain, doctrine.dbal.tenant_connection and related services for test container access

---
+++ tests/Integration/Testing/Support/MakeTenancyTestServicesPublicPass.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenancy\Bundle\Tests\Integration\Testing\Support;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Compiler pass that makes all tenancy and Doctrine services public so
+ * the InteractsWithTenancy trait can retrieve them from the test container.
+ */
+final class MakeTenancyTestServicesPublicPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $ids = [
+            'tenancy.context',
+            'tenancy.bootstrapper_chain',
+            'doctrine.dbal.tenant_connection',
+            'doctrine.orm.tenant_entity_manager',
+            'doctrine.orm.landlord_entity_manager',
+            'tenancy.doctrine_bootstrapper',
+            'tenancy.cache_adapter',
+        ];
+
+        foreach ($ids as $id) {
+            if ($container->hasDefinition($id)) {
+                $container->getDefinition($id)->setPublic(true);
+            } elseif ($container->hasAlias($id)) {
+                $container->getAlias($id)->setPublic(true);
+            }
+        }
+    }
+}
